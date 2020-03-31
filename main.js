d6a7ae059fd144e1f3caf35d1c341d94f8f2b671
evan-dimartinis <60354368+evan-dimartinis@users.noreply.github.com>
2020-03-31 18:50:33 -0400

    Light/Dark button added

--- main.js
+++ main.js
@@ -11,3 +11,17 @@ newImage.setAttribute('src', xxx);
 thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
+let dbutton = document.getElementById("darkbutton")
+let ld = dbutton.getAttribute("class")
+function changeLighting() {
+    if (ld==="dark") {
+        dbutton.setAttribute("class", "light");
+        dbutton.innerHTML("Darken");
+        overlay.style.background_color = "rgba(0,0,0,0.5)"
+    }
+    else {
+        dbutton.setAttribute("class", "dark");
+        dbutton.innerHTML("Lighten");
+        overlay.style.background_color = "rgba(0,0,0,0)"
+    }
+}
